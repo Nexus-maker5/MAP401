4dac1193b44c405bde336eb4f8db357afdbe3549
Nexus-maker5 <binafo143@gmail.com.com>
2026-04-06 21:53:06 +0200

    Tache3

--- Tache3/contour.h
+++ Tache3/contour.h
@@ -5,9 +5,6 @@
 #include "liste.h"
 #include <stdio.h>
 
-
-
-
 typedef enum {Nord,Est,Sud,Ouest} Orientation;
 
 typedef struct {
@@ -15,19 +12,25 @@ typedef struct {
   Orientation o;
 } robot;
 
-
+/*Trouver le pixel de depart (le premier pixel noir ayant un pixel blanc au dessus)*/
 Point trouver_pixel_depart(Image I);
 
+/*Avancer le robot d'une case */
 void avancer(robot *R);
 
+/* faire tourner le robot à gauche */
 void tourner_a_gauche(robot *R);
 
+/* faire tourner le robot à droite */
 void tourner_a_droite(robot *R);
 
-void memoriser_position(Contour *C,robot *R);
-
+/*Mettre a jour l'orientation du robot */
 void nouvelle_orientation(Image I,robot *R);
 
+/*Memoriser la position du robot*/
+void memoriser_position(Contour *C,robot *R);
+
+/*Calculer le contour de l'image*/
 void calculer_contour(Image I);
 
 
